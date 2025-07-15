cfe9448183f24cd3f801edea151152234389cdca
JoelVCrasta <joelcrasta03@proton.me>
2025-07-15 21:33:03 +0530

    added animation for description

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,7 +5,7 @@ import Container from "./Container"
 
 const Hero = () => {
   return (
-    <Container className="h-full flex flex-col justify-center">
+    <Container className="h-full flex flex-col justify-center pointer-events-none">
       <motion.div
         className="font-voxel leading-30"
         initial={{ opacity: 0, y: -40 }}
@@ -16,12 +16,19 @@ const Hero = () => {
         <p className="text-6xl">Developer</p>
       </motion.div>
 
-      <p className="text-3xl mt-4 max-w-[700] font-pixelify">
-        Hey there! I'm someone who enjoys building cool, functional
-        applications. I love working on anything that feels interesting or fun,
-        always exploring new ideas and experimenting with different
-        technologies.
-      </p>
+      <motion.div
+        className="font-voxel leading-30"
+        initial={{ opacity: 0, y: 40 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6, ease: "easeOut" }}
+      >
+        <p className="text-3xl mt-4 max-w-[700] font-pixelify">
+          Hey there! I'm someone who enjoys building cool, functional
+          applications. I love working on anything that feels interesting or
+          fun, always exploring new ideas and experimenting with different
+          technologies.
+        </p>
+      </motion.div>
     </Container>
   )
 }
