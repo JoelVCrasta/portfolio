9ef260c71a38be03d20146737d26ce1fd8990372
JoelVCrasta <joelcrasta03@proton.me>
2025-07-17 22:34:54 +0530

    added socials and arrow animation

---
+++ components/Social.tsx
@@ -0,0 +1,40 @@
+import { Github, Instagram, Linkedin } from "lucide-react"
+import Image from "next/image"
+
+const Social = () => {
+  return (
+    <section className="flex items-center justify-center gap-4 absolute top-10 left-10">
+      <Image src="/arrow.gif" alt="arrow" width={24} height={24} />
+      <a
+        href="https://github.com/JoelVCrasta"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Github size={24} />
+      </a>
+      <a
+        href="https://linkedin.com/in/joelvcrasta"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Linkedin size={24} />
+      </a>
+      <a
+        href="https://instagram.com/jole_jole_"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Instagram size={24} />
+      </a>
+      <Image
+        src="/arrow.gif"
+        alt="arrow"
+        width={24}
+        height={24}
+        style={{ transform: "scaleX(-1)" }}
+      />
+    </section>
+  )
+}
+
+export default Social
