7e438a547623f269399a84b96edcc911c0e386dd
JoelVCrasta <joelcrasta03@proton.me>
2025-07-22 00:31:14 +0530

    made responsive

--- components/MarqueeFooter.tsx
+++ components/MarqueeFooter.tsx
@@ -3,8 +3,8 @@ import Marquee from "react-fast-marquee"
 
 const MarqueeFooter = () => {
   return (
-    <div className="bg-foreground text-background flex pt-8 mt-8">
-      <div className="font-voxel font-bold text-9xl">
+    <div className="bg-foreground text-background flex pt-3 md:pt-4 lg:pt-8 mt-8 overflow-hidden">
+      <div className="font-voxel font-bold text-5xl md:text-7xl lg:text-9xl">
         <Marquee autoFill>Portfolio . Joel Crasta .&nbsp;</Marquee>
       </div>
     </div>
