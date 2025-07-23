944475c574c71ed6d50f3d9edca4bb4471e66bda
JoelVCrasta <joelcrasta03@proton.me>
2025-07-23 16:59:35 +0530

    updated responsiveness to center when in mobile view

--- components/Social.tsx
+++ components/Social.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 
 const Social = () => {
   return (
-    <section className="flex items-center justify-center gap-4 absolute top-10 left-10">
+    <section className="flex items-center justify-center gap-4 absolute top-10 left-1/2 -translate-x-1/2 md:-translate-x-0 md:left-10">
       <Image src="/arrow.gif" alt="arrow" width={24} height={24} />
       <a
         href="https://github.com/JoelVCrasta"
