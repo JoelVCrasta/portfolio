bb5bb627b226b31dd01c8f13efbb558254fb4aed
JoelVCrasta <joelcrasta03@proton.me>
2025-07-28 20:41:08 +0530

    added message component and some css

---
+++ components/Message.tsx
@@ -0,0 +1,69 @@
+const Message = () => {
+  return (
+    <section className="border-4 p-2 md:p-5 w-7/12">
+      <h2 className="font-pixelify text-2xl md:text-5xl lg:text-7xl font-bold mb-4">
+        Send me a message :3
+      </h2>
+
+      <form
+        action="https://formsubmit.co/loejstarc@gmail.com"
+        method="POST"
+        className="flex flex-col gap-4"
+      >
+        <div className="flex gap-4">
+          <div className="flex flex-col w-full">
+            <label
+              htmlFor="name"
+              className="font-pixelify font-bold text-2xl lg:text-3xl"
+            >
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              className="outline-none border-4 p-2 font-pixelify text-xl bg-accent"
+              required
+            />
+          </div>
+
+          <div className="flex flex-col w-full">
+            <label
+              htmlFor="email"
+              className="font-pixelify font-bold text-2xl lg:text-3xl"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="outline-none border-4 p-2 font-pixelify text-xl bg-accent"
+              required
+            />
+          </div>
+        </div>
+
+        <div className="flex flex-col">
+          <label
+            htmlFor="message"
+            className="font-pixelify font-bold text-2xl lg:text-3xl"
+          >
+            Message
+          </label>
+          <textarea
+            id="message"
+            name="message"
+            className="outline-none border-4 p-2 font-pixelify text-xl h-32 resize-none scrollbar bg-accent"
+            required
+            minLength={10}
+          />
+        </div>
+
+        <button className="bg-foreground text-background">Submit</button>
+      </form>
+    </section>
+  )
+}
+
+export default Message
