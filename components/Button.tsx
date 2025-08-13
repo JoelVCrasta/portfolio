1f28140d4d1da17a36c12049c16d43bb0cdd17a6
JoelVCrasta <joelcrasta03@proton.me>
2025-08-13 22:42:15 +0530

    increaser x padding

--- components/Button.tsx
+++ components/Button.tsx
@@ -6,7 +6,7 @@ interface ButtonProps {
 const Button = ({ text, className }: ButtonProps) => {
   return (
     <button
-      className={`relative px-2 py-3 bg-foreground text-background ${className}`}
+      className={`relative px-4 py-3 bg-foreground text-background ${className}`}
     >
       <div className="absolute w-2 h-2 bg-background top-0 right-0" />
       <div className="absolute w-2 h-2 bg-background top-0 left-0" />
