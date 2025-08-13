1a66303246cc1359e03e5cf0b174a61e27dc0052
JoelVCrasta <joelcrasta03@proton.me>
2025-08-13 23:02:15 +0530

    updated button for type and events

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,12 +1,21 @@
 interface ButtonProps {
   text: string
   className?: string
+  type?: "button" | "submit" | "reset"
+  onClick?: React.MouseEventHandler<HTMLButtonElement>
 }
 
-const Button = ({ text, className }: ButtonProps) => {
+const Button = ({
+  text,
+  className = "",
+  type = "button",
+  onClick,
+}: ButtonProps) => {
   return (
     <button
-      className={`relative px-4 py-3 bg-foreground text-background ${className}`}
+      className={`relative px-4 py-3 bg-foreground text-background hover:scale-105 transition ${className}`}
+      type={type}
+      onClick={onClick}
     >
       <div className="absolute w-2 h-2 bg-background top-0 right-0" />
       <div className="absolute w-2 h-2 bg-background top-0 left-0" />
