551e3dfed8826f30d65ce7a8c6aaaa8ba08d7042
JoelVCrasta <joelcrasta03@proton.me>
2025-08-13 22:42:04 +0530

    made projects hover animation faster

--- components/Projects/ProjectCard.tsx
+++ components/Projects/ProjectCard.tsx
@@ -21,11 +21,11 @@ const ProjectCard = ({
       className="group relative mx-auto px-2 md:px-5 xl:flex items-center justify-between hover:bg-foreground hover:text-background transition-colors duration-300 font-pixelify hidden"
     >
       <div className="relative h-full w-full overflow-hidden">
-        <h3 className="py-2 md:py-12 text-2xl font-bold tracking-tighter md:text-5xl lg:text-7xl duration-500 group-hover:-translate-y-full">
+        <h3 className="py-2 md:py-12 text-2xl font-bold tracking-tighter md:text-5xl lg:text-7xl duration-300 group-hover:-translate-y-full">
           {title}
         </h3>
 
-        <div className="absolute top-0 left-0 flex h-full flex-col justify-center translate-y-full duration-500 group-hover:translate-y-0">
+        <div className="absolute top-0 left-0 flex h-full flex-col justify-center translate-y-full duration-300 group-hover:translate-y-0">
           <h3 className="text-xl font-bold tracking-tighter md:text-5xl lg:text-7xl">
             {title}
           </h3>
